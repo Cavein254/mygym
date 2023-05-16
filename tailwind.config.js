b4c0a0013aaffa582fb1f709640ad0c5da76ddd2
johnmuthua <caveomuthama@gmail.com>
2023-05-16 09:31:43 +0300

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "gray-20":"#F8F4EB",
+        "gray-50":"#EFE6E6",
+        "gray-100":"#5E0000",
+        "primary-100":"#FFE1E0",
+        "primary-300":"#FFA6A3",
+        "primary-500":"#FF6BB6",
+        "secondary-400":"#FFCD58",
+        "secondary-500":"#FFC132",
+      }
+    },
   },
   plugins: [],
 }
