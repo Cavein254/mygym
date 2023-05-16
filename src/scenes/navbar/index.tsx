9eab564e8ec9e3c47614b64eda59f01c1ea23ce3
johnmuthua <caveomuthama@gmail.com>
2023-05-16 13:10:16 +0300

    Bug cleared linter errors

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,9 +1,7 @@
 import Logo from "@/assets/Logo.png";
 
 
-type Props = {}
-
-const Navbar = (props:Props) => {
+const Navbar = () => {
     const flexBetween = "flex items-center justify-between";
     return (
         <nav className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
