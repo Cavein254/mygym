edbdc1a21d703909bb7611e2f8e4aa4fb519a676
johnmuthua <caveomuthama@gmail.com>
2023-05-17 08:58:05 +0300

    Add ActionButton

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,5 +1,9 @@
 import Logo from "@/assets/Logo.png";
+import UseMediaQuery from "@/hooks/UseMediaQuery";
+import ActionButton from "@/shared/ActionButton";
 import { SelectedPage } from "@/shared/types";
+import { Bars3Icon } from "@heroicons/react/20/solid";
+import { useState } from "react";
 import Link from "./Link";
 
 type Props = {
@@ -9,12 +13,15 @@ type Props = {
 
 const Navbar = ({selectedPage,setSelectedPage}:Props) => {
     const flexBetween = "flex items-center justify-between";
+    const isAboveMediumScreens = UseMediaQuery("(min-width:1060px)");
+    const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
     return (
         <nav className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
         <div className={`${flexBetween} mx-auto w-5/6 `}>
         <div className={`${flexBetween} gap-16 w-full`}>
             <img alt="logo" src={Logo} />
-            <div className={`${flexBetween} w-full`}>
+            {isAboveMediumScreens ?(
+                <div className={`${flexBetween} w-full`}>
                 <div className={`${flexBetween} gap-8 text-sm`}>
                     <Link page="Home" 
                     selectedPage = {selectedPage}
@@ -34,10 +41,19 @@ const Navbar = ({selectedPage,setSelectedPage}:Props) => {
                     />
                 </div>
                 <div className= {`${flexBetween}`}>
+                    <div className="pr-8">
                     <p>Sign In</p>
-                    <button>Become a Member</button>
+                    </div>
+                    <ActionButton setSelectedPage = {setSelectedPage}>Become a Member</ActionButton>
                 </div>
             </div>
+            ):(
+                <button className="rounded-full bg-yellow-500 p-2"
+                    onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                        <Bars3Icon className="h-6 w-6 text-white" />
+                </button>
+            )
+        }
         </div>
         </div>
         </nav>
