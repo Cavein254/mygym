58fda4e4603aa600a487a678c88b7a62354d7d86
johnmuthua <caveomuthama@gmail.com>
2023-05-29 10:04:25 +0300

    Add ourClasses component

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -1,6 +1,48 @@
+import image1 from '@/assets/image1.png'
+import image2 from '@/assets/image2.png'
+import image3 from '@/assets/image3.png'
+import image4 from '@/assets/image4.png'
+import image5 from '@/assets/image5.png'
+import image6 from '@/assets/image6.png'
 import Htext from '@/shared/Htext'
-import { SelectedPage } from '@/shared/types'
+import { ClassType, SelectedPage } from '@/shared/types'
 import { motion } from 'framer-motion'
+import Class from './Class'
+
+const classes:Array<ClassType> = [
+    {
+        name:"weight training classes",
+        description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam animi dolorem at omnis cumque amet dolor a exercitationem fugiat ducimus error, delectus facilis, maiores magni sunt eos numquam optio eligendi!",
+        image:image1
+    },
+    {
+        name:"Yoga classes",
+        description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam animi dolorem at omnis cumque amet dolor a exercitationem fugiat ducimus error, delectus facilis, maiores magni sunt eos numquam optio eligendi!",
+        image:image2
+    },
+    {
+        name:"Ab Core classes",
+        description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam animi dolorem at omnis cumque amet dolor a exercitationem fugiat ducimus error, delectus facilis, maiores magni sunt eos numquam optio eligendi!",
+        image:image3
+    },
+    {
+        name:"Adventure classes",
+        description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam animi dolorem at omnis cumque amet dolor a exercitationem fugiat ducimus error, delectus facilis, maiores magni sunt eos numquam optio eligendi!",
+        image:image4
+    },
+    {
+        name:"Fitness classes",
+        description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam animi dolorem at omnis cumque amet dolor a exercitationem fugiat ducimus error, delectus facilis, maiores magni sunt eos numquam optio eligendi!",
+        image:image5
+    },
+    {
+        name:"Training classes",
+        description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam animi dolorem at omnis cumque amet dolor a exercitationem fugiat ducimus error, delectus facilis, maiores magni sunt eos numquam optio eligendi!",
+        image:image6
+    }
+]
+
+
 type Props = {
     setSelectedPage: (value:SelectedPage) => void;
 }
@@ -29,6 +71,13 @@ function OurClasses({setSelectedPage}: Props) {
                     </p>
                 </div>
             </motion.div>
+            <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden">
+                <ul className='w-[2800px] whitespace-nowrap'>
+                    {classes.map((item,index) => (
+                        <Class key={`${item.name}-${index}`} name={item.name} image={item.image} description={item.description}/>
+                    ))}
+                </ul>
+            </div>
         </motion.div>
     </section>
   )
